f33ceb901b244b7c98691772454b58083cf7d509
surendrarathore <ranbanka@surendras-MacBook-Air.local>
2023-08-25 11:59:03 +0530

    pojo class added and data.sql file added

---
+++ src/main/java/com/student/pojo/Student.java
@@ -0,0 +1,60 @@
+package com.student.pojo;
+
+import jakarta.persistence.*;
+
+@Entity(name="Student")
+@Table(name="student")
+public class Student {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="city")
+    private String city;
+    @Column(name="roll_no")
+    private Integer rollNo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(Integer rollNo) {
+        this.rollNo = rollNo;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO student (first_name, last_name, city,roll_no) VALUES
+  ('shubham', 'singh', 'shubham@gmail.com',101),
+  ('amit', 'kumar', 'amit@email.com',102);
